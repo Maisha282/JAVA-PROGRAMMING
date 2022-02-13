0057655497bc5d6b042da02a0fff22b1e29d8c06
Maisha282 <99607747+Maisha282@users.noreply.github.com>
2022-02-13 22:55:20 +0600

    Add files via upload

---
+++ TASK1.java
@@ -0,0 +1,16 @@
+
+public class TASK1 {
+
+	public static void main(String[] args) {
+		double radius = 5.5;
+		System.out.println("Enter radius: ");
+		double area = 3.1416*radius*radius;
+		double perimeter = 2*3.1416*radius;
+		System.out.println("Area of a Circle: "+area);
+		System.out.println("Perimeter of a Circle: "+perimeter);
+		
+		
+
+	}
+
+}
